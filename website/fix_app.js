741d97b9a16ee7f4f48a6819b594878f33601bc4
Labib Bin Shahed <103604925+la-b-ib@users.noreply.github.com>
2026-09-08 13:11:22 +0600

    Delete website directory

--- website/fix_app.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf-8');
-
-// We just need to remove the onOpenCtf prop passed to TerminalModal
-content = content.replace(/onOpenCtf=\{\(\) => \{\s*setTerminalOpen\(false\);\s*setCtfOpen\(true\);\s*\}\}/g, '');
-
-fs.writeFileSync('src/App.tsx', content);
